978d7d798620671c49d414043295948075d7d8fb
vinhpham0818 <vinhhvbd@gmail.com>
2019-05-27 20:24:11 +0700

    build project nsx

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <div>
+      <h1>Hello</h1>
+    </div>
+    <div>
+      <table width="100%" border="1">
+        <tr>
+          <th>ABC</th>
+          <th>DGH</th>
+        </tr>
+      </table>
+    </div>
+    </div>
+  );
+}
+
+export default App;
