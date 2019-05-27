a943bc9dffd61bbd7ed97b46968c0223e35e6b69
vinhpham0818 <vinhhvbd@gmail.com>
2019-05-27 21:09:44 +0700

    taofilecss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HeaderComp from './components/header-component';
 
 function App() {
   return (
     <div>
+      <div className="main-header">
+        <header>Header Componet</header>
+      </div>
     <div>
       <h1>Hello</h1>
     </div>
---
+++ src/components/header-component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './header-component.css';
+class HeaderComp extends React.Component {
+    render () {
+        return (
+            <div className="main-header">
+                <h1>Header Component</h1>
+            </div>
+        )
+    }
+    
+}
+
+export default HeaderComp
